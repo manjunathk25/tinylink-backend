511aa16f40fad9d5c70ee3b9cf9e452e694442dc
manjunath25 <k.mnjunth@gmail.com>
2025-11-22 00:03:19 +0530

    Add initial Prisma migration and update schema

---
+++ prisma/migrations/20251121183203_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "clicks" INTEGER NOT NULL DEFAULT 0,
+    "lastClicked" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_code_key" ON "Link"("code");
